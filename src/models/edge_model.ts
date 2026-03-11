89fb2d0954df5598daa5b93a06da5cc51171a027
Amit Aviv <amitaviv01@gmail.com>
2026-03-11 15:56:06 +0200

    removed interface

--- src/models/edge_model.ts
+++ src/models/edge_model.ts
@@ -1,10 +1,5 @@
 import {Codec, string} from 'purify-ts'
 
-export interface Edge {
-    sourrc_node_id : number;
-    second_node_id : number;
- }
-
 export const EdgeParamsCodec = Codec.interface({
     source_node_id: string,
     target_node_id: string,
--- src/models/node_model.ts
+++ src/models/node_model.ts
@@ -1,9 +1,9 @@
-import {Codec, number, GetType} from 'purify-ts'
+import {Codec, string} from 'purify-ts'
 
 export interface Node {
     node_id : number;
 }
 
 export const NodeParamsCodec = Codec.interface({
-    node_id : number
+    node_id : string
 })
